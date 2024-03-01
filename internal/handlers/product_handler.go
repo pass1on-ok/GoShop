874a1248886569dd801affcadd3e3f0de5cc5924
Kumar Oskenbay <123387665+pass1on-ok@users.noreply.github.com>
2024-03-02 00:52:20 +0600

    Create product_handler.go

---
+++ internal/handlers/product_handler.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/pass1on-ok/Golang-Project/pkg/product"
+)
+
+func GetProducts(w http.ResponseWriter, r *http.Request) {
+	products := product.GetAllProducts()
+	jsonData, err := json.Marshal(products)
+	if err != nil {
+		http.Error(w, "Error converting data to JSON", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
+}
