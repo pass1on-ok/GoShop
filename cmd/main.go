497be3a6e54d6f9a7871fae1b9b8d1a0390afb07
Kumar Oskenbay <123387665+pass1on-ok@users.noreply.github.com>
2024-03-02 01:57:17 +0600

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 
-	"onlinestore/internal/handlers"
+	"onlinestore/pkg/product"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/home", handlers.GetProducts).Methods("GET")
+
+	fs := http.FileServer(http.Dir("web-page"))
+	http.Handle("/web-page/", http.StripPrefix("/web-page/", fs))
+
+	r.HandleFunc("/", homeHandler).Methods("GET")
 
 	log.Println("Server is running on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	http.ListenAndServe(":8080", r)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+
+	products := product.GetAllProducts()
+
+	tmpl, err := template.ParseFiles("web-page/homepage/home.html")
+	if err != nil {
+		http.Error(w, "Error parsing HTML template", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, products)
+	if err != nil {
+		http.Error(w, "Error executing HTML template", http.StatusInternalServerError)
+		return
+	}
 }
