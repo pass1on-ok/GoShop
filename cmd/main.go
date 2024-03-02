4eaf27a61c2aa990046a0a3c29fd1fc575ca7e3a
Kumar <k_oskenbay@kbtu.kz>
2024-03-02 12:52:04 +0500

    Finished for Midterm

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,4 @@
+// cmd/main.go
 package main
 
 import (
@@ -20,20 +21,36 @@ func main() {
 	defer db.Close()
 
 	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS products (
-						id SERIAL PRIMARY KEY,
-						name TEXT,
-						price NUMERIC,
-						description TEXT,
-						quantity_in_stock INTEGER,
-						imagepath TEXT
-					)`)
+		id SERIAL PRIMARY KEY,
+		name TEXT,
+		price NUMERIC,
+		description TEXT,
+		quantity_in_stock INTEGER,
+		imagepath TEXT
+	)`)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	_, err = db.Exec(`INSERT INTO products (name, price, description, quantity_in_stock, imagepath)
-						VALUES ($1, $2, $3, $4, $5)`,
-		"Product Name", 19.99, "Product Description", 100, "https://resources.cdn-kaspi.kz/img/m/p/hc1/h65/83559848181790.png?format=preview-large")
+	products := []product.Product{
+		{Name: "Apple iPhone 15 Pro Max 256Gb серый", Price: 654857, Description: "Product Description 1", QuantityInStock: 12, ImagePath: "https://resources.cdn-kaspi.kz/img/m/p/hc1/h65/83559848181790.png?format=preview-large"},
+		{Name: "Apple iPhone 13 128Gb Midnight черный", Price: 299929, Description: "Product Description 2", QuantityInStock: 3, ImagePath: "https://resources.cdn-kaspi.kz/img/m/p/h32/h70/84378448199710.jpg?format=preview-large"},
+		{Name: "Apple iPhone 15 128Gb черный", Price: 401794, Description: "Product Description 3", QuantityInStock: 1, ImagePath: "https://resources.cdn-kaspi.kz/img/m/p/he2/h1d/83559338442782.png?format=preview-large"},
+		{Name: "Apple iPhone 11 128Gb Slim Box черный", Price: 244900, Description: "Product Description 4", QuantityInStock: 2, ImagePath: "https://resources.cdn-kaspi.kz/img/m/p/hd8/hac/63897052413982.jpg?format=preview-large"},
+		{Name: "Apple iPhone 13 128Gb белый", Price: 300000, Description: "Product Description 5", QuantityInStock: 12, ImagePath: "https://resources.cdn-kaspi.kz/img/m/p/hc9/h90/64209083007006.jpg?format=preview-large"},
+		{Name: "Apple iPhone 14 128Gb черный", Price: 341178, Description: "Product Description 6", QuantityInStock: 1, ImagePath: "https://resources.cdn-kaspi.kz/img/m/p/h98/h2b/64400497737758.jpg?format=preview-large"},
+		{Name: "Samsung Galaxy A24 6 ГБ/128 ГБ черный", Price: 91636, Description: "Product Description 7", QuantityInStock: 20, ImagePath: "https://resources.cdn-kaspi.kz/img/m/p/hdc/h12/80750151303198.jpg?format=preview-large"},
+		{Name: "Xiaomi Redmi 12 4G 8 ГБ/256 ГБ черный", Price: 68759, Description: "Product Description 8", QuantityInStock: 10, ImagePath: "https://resources.cdn-kaspi.kz/img/m/p/h75/hbc/81335343775774.png?format=preview-large"},
+		{Name: "Apple iPhone 15 Pro 256Gb синий", Price: 597788, Description: "Product Description 9", QuantityInStock: 30, ImagePath: "https://resources.cdn-kaspi.kz/img/m/p/h88/hde/83559835697182.jpg?format=preview-large"},
+		{Name: "Apple iPhone 14 128Gb starlight", Price: 341100, Description: "Product Description 10", QuantityInStock: 2, ImagePath: "https://resources.cdn-kaspi.kz/img/m/p/h9f/h49/64481569832990.jpg?format=preview-large"},
+		{Name: "Samsung Galaxy A54 5G 8 ГБ/256 ГБ черный", Price: 186756, Description: "Product Description 11", QuantityInStock: 11, ImagePath: "https://resources.cdn-kaspi.kz/img/m/p/h81/h13/80435536887838.jpg?format=preview-large"},
+		{Name: "Apple iPhone 13 128Gb зеленый", Price: 307856, Description: "Product Description 12", QuantityInStock: 10, ImagePath: "https://resources.cdn-kaspi.kz/img/m/p/hd1/h2f/64255724159006.jpg?format=preview-large"},
+		{Name: "Apple iPhone 15 128Gb голубой", Price: 397652, Description: "Product Description 13", QuantityInStock: 12, ImagePath: "https://resources.cdn-kaspi.kz/img/m/p/hd1/h07/83559339032606.png?format=preview-large"},
+		{Name: "Xiaomi Redmi 12C 4 ГБ/128 ГБ серый", Price: 48800, Description: "Product Description 14", QuantityInStock: 16, ImagePath: "https://resources.cdn-kaspi.kz/img/m/p/ha6/h82/69586957697054.jpg?format=preview-large"},
+		{Name: "Poco X6 Pro 12 ГБ/512 ГБ черный", Price: 172598, Description: "Product Description 15", QuantityInStock: 7, ImagePath: "https://resources.cdn-kaspi.kz/img/m/p/hbe/h45/84940376899614.jpg?format=preview-large"},
+		{Name: "Apple iPhone 15 128Gb черный", Price: 401794, Description: "Product Description 3", QuantityInStock: 1, ImagePath: "https://resources.cdn-kaspi.kz/img/m/p/he2/h1d/83559338442782.png?format=preview-large"},
+	}
+	err = product.AddProductsToDB(db, products)
 	if err != nil {
 		log.Fatal(err)
 	}
--- pkg/product/product.go
+++ pkg/product/product.go
@@ -35,3 +35,40 @@ func GetAllProductsFromDB(db *sql.DB) ([]Product, error) {
 
 	return products, nil
 }
+
+func AddProductsToDB(db *sql.DB, products []Product) error {
+	for _, product := range products {
+
+		exists, err := ProductExists(db, product.Name)
+		if err != nil {
+			return err
+		}
+
+		if !exists {
+			err := AddProductToDB(db, product)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func AddProductToDB(db *sql.DB, product Product) error {
+	_, err := db.Exec(`INSERT INTO products (name, price, description, quantity_in_stock, imagepath)
+						VALUES ($1, $2, $3, $4, $5)`,
+		product.Name, product.Price, product.Description, product.QuantityInStock, product.ImagePath)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ProductExists(db *sql.DB, productName string) (bool, error) {
+	var exists bool
+	err := db.QueryRow("SELECT EXISTS (SELECT 1 FROM products WHERE name = $1)", productName).Scan(&exists)
+	if err != nil {
+		return false, err
+	}
+	return exists, nil
+}
