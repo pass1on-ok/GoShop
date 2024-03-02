471ab57e0c33ea4d09b2f585bcc1069e036bda8e
Kumar Oskenbay <123387665+pass1on-ok@users.noreply.github.com>
2024-03-02 12:26:59 +0600

    Delete sql directory

--- sql/create_tables.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
-    name TEXT,
-    price NUMERIC,
-    description TEXT,
-    quantity_in_stock INTEGER
-);
--- sql/initial_data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO products (name, price, description, quantity_in_stock)
-VALUES ('Product 1', 10.99, 'Description of Product 1', 100);
-
-INSERT INTO products (name, price, description, quantity_in_stock)
-VALUES ('Product 2', 19.99, 'Description of Product 2', 50);
-
-INSERT INTO products (name, price, description, quantity_in_stock)
-VALUES ('Product 3', 29.99, 'Description of Product 3', 200);
